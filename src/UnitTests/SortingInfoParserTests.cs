07904c46af37866af60f8805cac8679408d1e4eb
NotIlya <secretman44444@gmail.com>
2023-05-04 03:07:26 +0300

    Add tests for SortingInfoParser

---
+++ src/UnitTests/SortingInfoParserTests.cs
@@ -0,0 +1,39 @@
+﻿using Api.Controllers.ProductsControllers.Helpers;
+using Infrastructure.FilteringSystem.Product;
+using Infrastructure.SortingSystem;
+using Infrastructure.SortingSystem.Product;
+
+namespace UnitTests;
+
+public class SortingInfoParserTests
+{
+    private readonly SortingInfoParser _parser;
+    
+    public SortingInfoParserTests()
+    {
+        _parser = new SortingInfoParser();
+    }
+
+    [Fact]
+    public void ParseProductSortingInfo_PassValidSorting_ParsedSortingInfo()
+    {
+        var rawSortingInfo = new List<string>()
+        {
+            "+name",
+            "-name",
+            "+price",
+            "-price"
+        };
+        var expect = new List<ProductSorting>()
+        {
+            new ProductSorting(ProductSortingProperty.Name, SortingSide.Asc),
+            new ProductSorting(ProductSortingProperty.Name, SortingSide.Desc),
+            new ProductSorting(ProductSortingProperty.Price, SortingSide.Asc),
+            new ProductSorting(ProductSortingProperty.Price, SortingSide.Desc)
+        };
+
+        List<ProductSorting> result = _parser.ParseProductSortingInfo(rawSortingInfo);
+        
+        Assert.Equal(expect, result);
+    }
+}
